c1cadf5bbc415f5eab4da00bbc5d0484eca6b1d3
Kokemater <kokesn05@gmail.com>
2024-10-27 23:25:10 +0100

    sfd

--- ft_printf/print_text.c
+++ ft_printf/print_text.c
@@ -1,5 +1,6 @@
 #include "ft_printf.h"
 #include "Libft/libft.h"
+
 int	put_char_with_return(char c)
 {
     return (write(1,&c,1));
@@ -9,11 +10,11 @@ int	ft_print_percent(void)
 	return (write(1,"%%",1));
 }
 
-
 int	put_str_with_return(char *s)
 {
 	int	len;
-
+	if (!s)
+		return (write(1, "(null)", 6));
 	len = ft_strlen(s);
 	ft_putstr_fd(s,1);
 	return (len);
