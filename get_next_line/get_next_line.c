bd531236cb675cee48819ffd38720dd5405ed4a9
Kokemater <kokesn05@gmail.com>
2024-11-19 23:22:28 +0100

    sdf

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,95 +1,93 @@
 #include "get_next_line.h"
 
-size_t  ft_strlen(const char *str)
+char	*ft_strdup(const char *src)
 {
-        size_t  i;
-
-        i = 0;
-        while (str[i] != 0)
-        {
-                i++;
-        }
-        return (i);
-}
-static void    *ft_memset(void *ptr, int val, size_t num)
-{
-        size_t                  i;
-
-        i = 0;
-        while (i < num)
-        {
-                ((unsigned char *) ptr)[i] = val;
-                i++;
-        }
-        return (ptr);
-}
-
-static void    *ft_calloc(size_t nmemb, size_t size)
-{
-        void    *new;
-
-        new = (char *) malloc(nmemb * size);
-        if (!new)
-                return (NULL);
-        ft_memset(new, 0, nmemb * size);
-        return (new);
-}
-
-char    *ft_strjoin(char const *s1, char const *s2)
-{
-        int             total_space;
-        char    *result;
-        int             i;
-        int             j;
-
-        i = 0;
-        j = 0;
-        total_space = ft_strlen(s1) + ft_strlen(s2);
-        result = (char *) malloc(sizeof(char) * total_space + 1);
-        if (!result)
-                return (NULL);
-        while (s1[i] != 0)
-        {
-                result[i] = s1[i];
-                i++;
-        }
-        while (s2[j] != 0)
-        {
-                result[i] = s2[j];
-                i++;
-                j++;
-        }
-        result[i] = 0;
-        return (result);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	int	i;
+	int		n;
+	char	*dest;
+	int		i;
 
 	i = 0;
-	while (s[i] != 0)
+	n = ft_strlen(src);
+
+	dest = (char *) malloc(sizeof(char) * (n + 1));
+	if (!dest)
+		return (NULL);
+	dest[n] = 0;
+	while (src[i])
 	{
-		if (s[i] == (char) c)
-			return ((char *)(s + i));
+		dest[i] = src[i];
 		i++;
 	}
-	if ((char) c == s[i])
-		return ((char *)(s + i));
-	return (0);
+	return (dest);
+}
+
+char	*read_until_n(char *big_buff, int fd, int *end)
+{
+	int bytes_read;
+	char tmp_buff[BUFFER_SIZE + 1];
+	char	*big_buff_tmp;
+
+	if (!big_buff)
+		big_buff = calloc(1,1);
+	while (!ft_strchr(big_buff, '\n'))
+	{
+		bytes_read = read(fd, tmp_buff, BUFFER_SIZE);
+		if (bytes_read <= 0)
+		{
+			*end = 1;
+			return (big_buff);
+		}
+		tmp_buff[bytes_read] = 0;
+		big_buff_tmp = ft_strjoin(big_buff, tmp_buff);
+		free(big_buff);
+		big_buff = big_buff_tmp;
+		if (!big_buff)
+			return ( NULL);
+	}
+	return (big_buff);
+	
+}
+
+char	*extract_line(char **big_buff)
+{
+	int		n_position;
+	char	*line;
+	char	*big_buff_cpy;
+	char	*n;
+
+	n = ft_strchr(*big_buff, '\n');
+	n_position = (int) (n - *big_buff);
+	line = ft_strndup(*big_buff, n_position + 1);
+	if (!line)
+		return (NULL);
+	big_buff_cpy = ft_strdup(*big_buff);
+	if (!big_buff_cpy)
+		return (free(line), NULL);
+	free(*big_buff);
+	*big_buff = ft_strdup(big_buff_cpy + n_position + 1);
+	if (!(*big_buff))
+		return (free(big_buff_cpy), free(line), NULL);
+	return (free(big_buff_cpy), line);
+
 }
 
 char	*get_next_line(int fd)
 {
-	static char *buffer;
-	char *buff;
+	static char *big_buff;
+	static int	end;
+	char	*ans;
 
-	buffer = (char *) ft_calloc(1,sizeof(char));
-	buff = (char *) ft_calloc(BYTES_READ + 1, sizeof(char));
-	while(ft_strchr(buffer, '\n'))
-	{
-		read(fd, buff, BYTES_READ);
-		buffer = ft_strjoin(buffer, buff);
-	}
-	return (buffer);
+	if (end)
+		return (NULL);
+	if (fd < 0 || BUFFER_SIZE < 0)
+		return (NULL);
+	big_buff = read_until_n(big_buff, fd, &end);
+	if (!big_buff)
+		return (NULL);
+	if (end)
+		return (big_buff);
+	ans = extract_line(&big_buff);
+	if (!ans)
+		return (NULL);
+	return (ans);
 }
--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,11 +6,14 @@ int main(void)
 {
 	int fd;
 	char *next_line;
-	int count;
 
-	printf("holay\n");
-	count = 0;
-	fd = open("texto.txt", O_RDONLY);
-	next_line = get_next_line(fd);
-	printf("%s", next_line);
+	fd = open("text.txt", O_RDONLY);
+	int i = 0;
+	while ((next_line = get_next_line(fd)) != 0)
+	{
+		printf("[line %d] = '%s' \n", ++i, next_line);
+		free(next_line);
+	}
+	close(fd);
+
 }
